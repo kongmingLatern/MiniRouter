c3c920082d6f15b22cd91e407f4e5d56c1f5624c
施颖杰 <1577310596@qq.com>
2022-07-10 21:20:09 +0800

    feat: add Link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Outlet } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 export default function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route index element={<Home />}></Route>
-          <Route path="product" element={<Product />}></Route>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />}></Route>
+            <Route path="product" element={<Product />}></Route>
+          </Route>
         </Routes>
       </Router>
     </div>
   )
 }
 
+function Layout() {
+  return (
+    <>
+      <h1>Layout</h1>
+      <Link to="/" style={{ "marginRight": "20px" }}>首页</Link>
+      <Link to="/product">商品</Link>
+      <Outlet />
+    </>
+  )
+}
 
 function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+    </>
   )
 }
 function Product() {
   return (
-    <h1>Product</h1>
+    <>
+      <h1>Product</h1>
+    </>
   )
 }
