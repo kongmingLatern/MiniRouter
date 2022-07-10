84ce1d50ce886795785160c2229c65bef762c0db
施颖杰 <1577310596@qq.com>
2022-07-10 21:05:06 +0800

    feat: add router6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+
+function Home() {
+  return (
+    <h1>Home</h1>
+  )
+}
+function Product() {
+  return (
+    <h1>Product</h1>
   )
 }
