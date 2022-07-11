eabcdf4929d6aaced7ce5f77b26f4b646b34aecc
施颖杰 <1577310596@qq.com>
2022-07-11 16:46:02 +0800

    change: some formats

--- src/mini-react-router/useRoutes.js
+++ src/mini-react-router/useRoutes.js
@@ -2,7 +2,6 @@ export default function useRoutes(routes) {
   const pathName = window.location.pathname
 
   return routes.map(route => {
-
     const match = pathName === route.path || pathName === '/' + route.path
     return match ? route.element : null
   })
