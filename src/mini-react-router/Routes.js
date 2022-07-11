fb79b75eb98924b7eea494fc3f8c0de9532b5142
施颖杰 <1577310596@qq.com>
2022-07-11 16:28:57 +0800

    feat: create Routes

--- src/mini-react-router/Routes.js
+++ src/mini-react-router/Routes.js
@@ -1,7 +1,7 @@
-export default function Routes() {
-  return (
-    <>
-      <div>Routes</div>
-    </>
-  )
+import { createRoutesFromChildren } from "./createRoutesFromChildren"
+import useRoutes from "./useRoutes"
+
+export default function Routes({ children }) {
+  const routes = createRoutesFromChildren(children)
+  return useRoutes(routes)
 };
