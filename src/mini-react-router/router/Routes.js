bf7695a7a5d4fbd3b113af1b465bf2be1390be42
施颖杰 <1577310596@qq.com>
2022-07-12 20:54:41 +0800

    fix: import path error

---
+++ src/mini-react-router/router/Routes.js
@@ -0,0 +1,7 @@
+import { createRoutesFromChildren } from "../createRoutesFromChildren"
+import { useRoutes } from "../hooks"
+
+export default function Routes({ children }) {
+  const routes = createRoutesFromChildren(children)
+  return useRoutes(routes)
+};
