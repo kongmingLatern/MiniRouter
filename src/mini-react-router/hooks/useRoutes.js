c67598c793ab714bae26a8b714d471c65ad00357
施颖杰 <1577310596@qq.com>
2022-07-12 22:20:32 +0800

    fix: some formats

--- src/mini-react-router/hooks/useRoutes.js
+++ src/mini-react-router/hooks/useRoutes.js
@@ -1,11 +1,21 @@
 export default function useRoutes(routes) {
+  /* 
+  routes
+   [{...}]
+    children: [{...}, {...}]
+    element: {$$typeof:symbol(react.element)}
+    path: '/' 
+  */
+  // console.log(routes);
   const pathName = window.location.pathname
 
   return routes.map(route => {
     // const match = pathName === route.path || pathName === '/' + route.path
+    // console.log('route map 遍历', route);
+    // 匹配父路由
     const match = pathName.startsWith(route.path)
 
-    console.log('route', pathName, route);
+    // console.log('route', pathName, route);
 
     return match ? route.element : null
   })
