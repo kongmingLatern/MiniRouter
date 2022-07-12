8ca7ca84ef95805b65e0bbd62d9b1e425f365a05
施颖杰 <1577310596@qq.com>
2022-07-12 14:41:33 +0800

    feat: add Link

--- src/mini-react-router/index.js
+++ src/mini-react-router/index.js
@@ -1,10 +1,12 @@
 import BrowserRouter from "./BrowserRouter";
 import Routes from "./Routes";
 import Route from "./Route";
+import Link from "./Link";
 
 export {
   BrowserRouter,
   Routes,
-  Route
+  Route,
+  Link
 }
 
