ca3a4417e8ca5be0e38f0de52bb05b5b8f037522
施颖杰 <1577310596@qq.com>
2022-07-12 15:21:04 +0800

    feat: add useNavigator To BrowserRouter

--- src/mini-react-router/BrowserRouter.js
+++ src/mini-react-router/BrowserRouter.js
@@ -1,3 +1,16 @@
+import React from 'react'
+import { createBrowserHistory } from "history";
+import Router from "./Router";
+
 export default function BrowserRouter({ children }) {
-  return children
+  // 组件卸载之前
+  let historyRef = React.useRef(null)
+
+  if (historyRef.current === null) {
+    historyRef.current = createBrowserHistory()
+  }
+
+  const history = historyRef.current
+
+  return <Router children={children} navigator={history} />
 };
