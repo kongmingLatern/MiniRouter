347390fce9d943ad2538862d0773cde2cb3414c0
施颖杰 <1577310596@qq.com>
2022-07-15 22:14:59 +0800

    feat: add loginOut

--- src/App.js
+++ src/App.js
@@ -123,9 +123,18 @@ function RequireAuth({ children }) {
 
 
 function User() {
+  const auth = useAuth()
+  const navigate = useNavigate()
   return (
     <div>
       <h1>User</h1>
+      <p>{auth.user?.username}</p>
+      <button onClick={() => {
+        auth.signout(navigate('/')
+        )
+      }}>
+        退出登录
+      </button>
     </div>
   )
 }
