4b6615cda38ed954b29bc72357b197c2cd632409
施颖杰 <1577310596@qq.com>
2022-07-18 22:02:15 +0800

    feat: happy path useMatch

--- src/mini-react-router/hooks/useMatch.js
+++ src/mini-react-router/hooks/useMatch.js
@@ -1,3 +1,6 @@
-export default function useMatch() {
-  return
+import { matchPath } from 'react-router-dom';
+import useLocation from './useLocation';
+export default function useMatch(pattern) {
+  const { pathname } = useLocation()
+  return matchPath(pattern, pathname)
 };
